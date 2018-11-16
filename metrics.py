416effebdee4e96662066cbed24ba76f5d50ed92
Siddhant Navali <sn446@cornell.edu>
2018-11-15 21:41:18 -0500

    Added metrics file

---
+++ metrics.py
@@ -0,0 +1,36 @@
+"""Keras 1.0 metrics.
+This file contains the precision, recall, and f1_score metrics which were
+removed from Keras by commit: a56b1a55182acf061b1eb2e2c86b48193a0e88f7
+"""
+from keras import backend as K
+
+
+def precision(y_true, y_pred):	
+    """Precision metric.	
+    Only computes a batch-wise average of precision. Computes the precision, a
+    metric for multi-label classification of how many selected items are
+    relevant.
+    """	
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))	
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))	
+    precision = true_positives / (predicted_positives + K.epsilon())	
+    return precision
+
+def recall(y_true, y_pred):	
+    """Recall metric.	
+    Only computes a batch-wise average of recall. Computes the recall, a metric
+    for multi-label classification of how many relevant items are selected.	
+    """	
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))	
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))	
+    recall = true_positives / (possible_positives + K.epsilon())	
+    return recall
+
+def f1_score(y_true, y_pred):
+    """Computes the F1 Score
+    Only computes a batch-wise average of recall. Computes the recall, a metric
+    for multi-label classification of how many relevant items are selected.	
+    """
+    p = precision(y_true, y_pred)
+    r = recall(y_true, y_pred)
+return (2 * p * r) / (p + r + K.epsilon())
